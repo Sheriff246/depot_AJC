b7f6873586c882067056d58d6064e5338e4d877e
Sheriff246 <160516524+Sheriff246@users.noreply.github.com>
2024-10-11 14:24:07 +0200

    Add files via upload

---
+++ gestion_users.sh
@@ -0,0 +1,26 @@
+$choix
+$user0
+while true ; do
+echo "Menu de gestion des utilisateurs"
+echo "1. Creer un utilisateur"
+echo "2. Supprimer un utilisateur"
+echo "3. Quitter"
+echo -----------------------------------
+echo "Choisissez une option 1,2,3 :"
+read -n 2 $choix
+if [ $choix=1 ]; then 
+echo "entrez le nom de l'utilisateur"
+read $user0
+useradd $user0
+fi
+if [ $choix=2 ] ; then
+echo "entre le nom de l'utilisateur"
+read $user0
+userdel $user0
+fi
+if [ $choix=3 ]
+exit
+if
+$choix!=1||2||3
+echo "la saisie est incorecte, veillez saisir votre choix : 1, 2, 3."
+read -n 2 $choix
